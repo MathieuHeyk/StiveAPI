7e9b0d56a72c92141886ea00c5eec785da767120
Mathieu Heyk <mathieu.heyk@outlook.fr>
2023-01-02 11:34:13 +0100

    [ADD] Connexion à la base de données

--- StiveAPI/Models/Article.cs
+++ StiveAPI/Models/Article.cs
@@ -1,25 +1,24 @@
-﻿namespace StiveAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StiveAPI.Models
 {
 	public class Article
 	{
+		[ Key ]
 		public int Id { get; set; }
+
+		[Required]
 		public string Nom { get; set; }
+
+		[Required]
 		public double Prix_unitaire { get; set; }
+		[Required]
 		public double Prix_carton { get; set; }
+		[Required]
 		public int Annee { get; set; }
+		[Required]
 		public int ID_famille { get; set; }
+		[Required]
 		public int ID_fournisseur { get; set; }
-
-
-		public Article( int id, string nom, double prix_unitaire, double prix_carton, int annee, int id_famille, int id_fournisseur )
-		{
-			Id = id;
-			Nom = nom;
-			Prix_unitaire = prix_unitaire;
-			Prix_carton = prix_carton;
-			Annee = annee;
-			ID_famille = id_famille;
-			ID_fournisseur = id_fournisseur;
-		}
 	}
 }
