c081b36490a905308a9292225ab910bf55e579e7
MathieuHeyk <58843227+MathieuHeyk@users.noreply.github.com>
2023-01-03 10:06:06 +0100

    Amélioration lisibilité (model inventaire)

--- StiveAPI/Models/Inventaire.cs
+++ StiveAPI/Models/Inventaire.cs
@@ -5,12 +5,15 @@ namespace StiveAPI.Models
     public class Inventaire
     {
         [Key]
-        public int Id_Inventaire { get; set; }
+        public int Id_inventaire { get; set; }
+        
         [Required]
         public int Quantite { get; set; }
+        
         [Required]
         public int Date { get; set; }
+        
         [Required]
-        public int Id_Utilisateur { get; set; }
+        public int Id_utilisateur { get; set; }
     }
 }
