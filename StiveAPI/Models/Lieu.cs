13cf3b5b52cfd939e00b193b993113647dbc23d7
FreeKy01 <fadykablouti@yahoo.it>
2023-01-04 09:59:03 +0100

    ajout classe lieu

---
+++ StiveAPI/Models/Lieu.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace StiveAPI.Models
+{
+    public class Lieu
+    {
+        [Key]
+        public int Id_lieu { get; set; }
+
+        [Required]
+        public string Numero { get; set; }
+        
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Nom { get; set; }
+
+        [Required]
+        public string Ville { get; set; }
+
+        [Required]
+        public string Code_postal { get; set; }
+
+        [Required]
+        public string Pays { get; set; }    
+    }
+
+}
